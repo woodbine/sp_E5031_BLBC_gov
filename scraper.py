d4da079ad7f8b7c09ee10a5c7084a52141b03f06
woodbine <ian@spendnetwork.com>
2015-04-13 13:09:44 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -31,11 +31,12 @@ for block in blocks:
 	link = block.a['href']
 	# add the right prefix onto the url
 	pageUrl = link.replace("/citizen-home","https://www.barnet.gov.uk/citizen-home")
+	print type(pageUrl)
+	'''
 	pageUrl = urllib.quote(pageUrl).encode('Latin-1') #  changes chars to percentages for use in a url
 	
 	print pageUrl
 	
-	'''
 	html2 = urllib2.urlopen(pageUrl)
 	soup2 = BeautifulSoup(html2)
 	fileBlocks = soup2.findAll('li')
